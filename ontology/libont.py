a530c095d03fecf0ead33e82d25cfaa4703d9245
steven <chenglin.cn@163.com>
2018-11-12 11:32:10 +0800

    add str transform to int. only 10 scale

--- ontology/libont.py
+++ ontology/libont.py
@@ -1,4 +1,4 @@
-from ontology.builtins import append
+from ontology.builtins import append, len, range, reversed
 
 def list_remove_elt(l, elt):
     nl = []
@@ -13,3 +13,21 @@ def elt_in(l, elt):
         if elt is i:
             return True
     return False
+
+def int(arg):
+    slen = len(arg)
+    n = 1
+    num = 0
+    scale = 10
+    elt = ['0','1','2','3','4','5','6','7','8','9', '-']
+    for i in reversed(range(0, slen)):
+        assert(elt_in(elt, arg[i: i+1]))
+        if i != 0 and arg[i: i+1] == '-':
+            assert(False)
+        if i == 0 and arg[i: i+1] == '-':
+            num = -num
+            break
+        else:
+            num += (arg[i: i+1] - '0') * n
+        n = n * scale
+    return num
