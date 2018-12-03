4785a57914c4d9076c105ab7a7afed24f366407d
steven <chenglin@ont.com>
2018-12-03 17:32:28 +0800

    make test_global more complicate

--- testdata/test/test_global.py
+++ testdata/test/test_global.py
@@ -36,4 +36,42 @@ def main():
     for i in range(0,len(h)):
         assert(h[i] == y[i])
         print(i)
+
+    test()
     print("all done")
+
+def test():
+    c = b[2]
+    d = b[3]
+    e = b[4]
+    f = b[5]
+    g = b['wo']
+    assert(c == 'hello')
+    assert(d == 'world')
+    assert(e == x)
+    print(c)
+    print(d)
+    print(e)
+    #print(f)
+    for i in range(0,len(f)):
+        assert(f[i] == y[i])
+        print(i)
+
+    assert(g[a[1]] == 'hello')
+    assert(g[a[2]] == 'world')
+    assert(g[a[3]] == x)
+    print(g[a[1]])
+    print(g[a[2]])
+    print(g[a[3]])
+    h = g[a[4]]
+    for i in range(0,len(h)):
+        assert(h[i] == y[i])
+        print(i)
+
+    test0()
+
+def test0():
+    a = 1
+    b = 2
+    c = a + b
+    assert(c == 3)
