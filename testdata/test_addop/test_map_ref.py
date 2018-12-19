92e9a4ba46fce3db9bd5897d9ae15a736e0ce313
steven <chenglin@ont.com>
2018-12-19 12:00:31 +0800

    add map values ref test

---
+++ testdata/test_addop/test_map_ref.py
@@ -0,0 +1,16 @@
+OntCversion = '2.0.0'
+from ontology.builtins import values, keys, print
+
+def Main():
+    map0 = {0:[9,8,7], 1:[6,5,4], 2:[3,2,1]}
+    values = map0.values()
+    assert(values[0][1] == 8)
+    assert(values[1][1] == 5)
+    assert(values[2][1] == 2)
+
+    values[0][1] = 108 + 0
+    values[1][1] = 105 + 0
+    values[2][1] = 103 + 0
+    assert(map0[0][1] == 108)
+    assert(map0[1][1] == 105)
+    assert(map0[2][1] == 103)
