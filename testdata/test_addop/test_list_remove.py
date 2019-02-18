e0d16b59fb21256e00f6dc5fabee6137dd21e4a2
steven <chenglin@ont.com>
2019-02-18 14:45:20 +0800

    update test_list_remove.py

--- testdata/test_addop/test_list_remove.py
+++ testdata/test_addop/test_list_remove.py
@@ -1,8 +1,10 @@
 OntCversion = '2.0.0'
-from ontology.builtins import remove, print, len
 
 def Main():
     l = [9,8,7,6,5]
+    a = l
+    assert(len(a) == 5)
+    print(len(a))
     for v in l:
         print(v)
 
@@ -17,3 +19,10 @@ def Main():
     assert(len(l) == 1)
     l.remove(0)
     assert(len(l) == 0)
+
+    print(len(a))
+    assert(len(a) == 2)
+    assert(a[0] == 6)
+    print(a[0])
+    print(a[1])
+    assert(a[1] == 5)
