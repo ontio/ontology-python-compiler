3128a72b8d65e79be15d31cb3d96a087c0c83c6d
steven <chenglin.cn@163.com>
2019-03-20 10:33:08 +0800

    fixed debug.json funcname error when across function (#42)

--- ontology/libont.py
+++ ontology/libont.py
@@ -86,6 +86,9 @@ def str_scale(arg_int, scale):
     if arg_int < 0:
         negtive = True
         arg_int = abs(arg_int)
+    elif arg_int == 0:
+        res = '0'
+        return res
 
     res = ''
     trans_map = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 0xa: 'a', 0xb: 'b', 0xc: 'c', 0xd: 'd', 0xe: 'e', 0xf: 'f'}
