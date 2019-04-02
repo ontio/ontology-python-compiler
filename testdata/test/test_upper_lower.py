6e51a6422d8b4d52f8c7bd44cf48abd977e15080
steven <chenglin@ont.com>
2019-04-02 11:07:31 +0800

    add upper/lower case in libont

---
+++ testdata/test/test_upper_lower.py
@@ -0,0 +1,13 @@
+from ontology.libont import upper, lower
+
+def Main():
+    s = '123abcdefghijklmnopqrstuvwxyz_*?/.####_+-ABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%^&*()!'
+    print(s)
+    res = upper(s)
+    print(res)
+
+    res0 = lower(s)
+    print(res0)
+
+    res1 = upper(res0)
+    assert(res == res1)
