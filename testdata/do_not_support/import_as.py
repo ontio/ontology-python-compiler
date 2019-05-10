a424ebcde244e1514a13aa635c298985bfc04917
Wyatt Mufson <wyatt@ryucoin.com>
2019-05-09 20:10:21 -0700

    Removed unused folder (#55)

--- testdata/do_not_support/import_as.py
+++
@@ -1,4 +0,0 @@
-from ontology.interop.System.Runtime import Log as mylog
-
-def Main():
-    pass
--- testdata/do_not_support/import_only.py
+++
@@ -1,5 +0,0 @@
-import ontology.interop.System.Runtime, ontology.interop.System.Blockchain, ontology.interop.System.Header
-import ontology.interop.System.Storage 
-
-def Main():
-    pass
