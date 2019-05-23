0a40c22f752ca34d68ccfef149390da6cb88450e
Wyatt Mufson <wyatt@ryucoin.com>
2019-05-23 21:29:25 +0300

    Updated testing script

--- ontology_test/runall.bash
+++ ontology_test/runall.bash
@@ -1,7 +1,33 @@
 #!/usr/bin/env bash
 
+total=0
+correct=0
+
+function success {
+  printf "\e[32m$1\e[m\n"
+}
+
+function error {
+  printf "\e[31m$ERROR $1\e[m\n"
+}
+
 for f in $(ls *.py)
 do
 	own=$0
-	[[ $f != ${own##*/} ]] &&  ../run.py -n $f -m 1
+	x=$([[ $f != ${own##*/} ]] && ../run.py -n $f -m 1 -t)
+	total=$[$total+1]
+	if [ $x == 1 ]
+	then
+		correct=$[$correct+1]
+		success "$f"
+	else
+		error "$f"
+	fi
 done
+echo "$correct/$total Tests Passed"
+if [ $correct == $total ]
+then
+	return 1
+else
+	return 0
+fi
