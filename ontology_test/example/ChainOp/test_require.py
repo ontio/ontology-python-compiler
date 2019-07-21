cc043456bc78fd167c74c48e39e1dea4c7d7c96f
Wyatt Mufson <wyatt@ryucoin.com>
2019-07-21 11:33:40 -0700

    Add require test

---
+++ ontology_test/example/ChainOp/test_require.py
@@ -0,0 +1,14 @@
+OntCversion = '2.0.0'
+from ontology.lib.require import Require, RequireIsAddress, RequireWitness
+
+
+def Main(operation, args):
+    if operation == "requireTest":
+        Require(len(args) == 1)
+        return requireTest(args[0])
+
+
+def requireTest(address):
+    RequireIsAddress(address)
+    RequireWitness(address)
+    return True
